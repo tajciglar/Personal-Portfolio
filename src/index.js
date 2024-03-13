222924688152a9680720e01b5a789d283c84b59b
tajciglar <taj.brzovic@gmail.com>
2024-03-13 15:42:32 +0100

    Add project section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import _ from 'lodash';
+
+
+/*
+let sections = document.querySelectorAll('.section');
+
+let currentSection = 0;
+
+function scrollHandler(event) {
+    console.log(event)
+    if(event.deltaY > 0 && currentSection < sections.length -1) {
+         pageUp();
+        currentSection++;
+    }
+ }
+
+function pageUp(){
+    sections[currentSection].classList.add('pageUp');
+    
+    setTimeout(function (){
+        sections[currentSection].classList.remove('pageUp');
+    }, 1000);
+    
+        
+  
+}
+
+document.addEventListener('wheel', _.throttle(scrollHandler, 1000));
+
+*/
